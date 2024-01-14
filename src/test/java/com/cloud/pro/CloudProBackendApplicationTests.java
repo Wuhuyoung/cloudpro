61092328439b11f53ef02af7b435d9f9be876bf2
wuhuyoung <1138841120@qq.com>
2024-01-14 15:45:14 +0800

    init project

---
+++ src/test/java/com/cloud/pro/CloudProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
